f68353481dfaa598618b281c2536591c89497fe0
Robert Schmidt <robert.schmidt@eurecom.fr>
2018-04-19 10:51:56 +0200

    slice configuration: update apidoc

--- src/north_api/flexible_sched_calls.cc
+++ src/north_api/flexible_sched_calls.cc
@@ -46,41 +46,271 @@ void flexran::north_api::flexible_sched_calls::register_calls(Pistache::Rest::Ro
   Pistache::Rest::Routes::Post(router, "/dl_sched/:sched_type", Pistache::Rest::Routes::bind(&flexran::north_api::flexible_sched_calls::change_scheduler, this));
 
   /**
-   * @api {post} /rrm_config/ Post a user-defined RAN policy command
-   * @apiName ApplyPolicyString
-   * @apiGroup user/slice/BS policies
+   * @api {post} /slice/enb/:id? Post a slice configuration
+   * @apiName ApplySliceConfiguration
+   * @apiGroup SliceConfiguration
+   * @apiParam {Number} [id=-1] The ID of the agent for which to change the
+   * slice configuration. This can be one of the following: -1 (last added
+   * agent), the eNB ID (in hex or decimal) or the internal agent ID which can
+   * be obtained through a `stats` call. Numbers smaller than 1000 are parsed as
+   * the agent ID.
    *
-   * @apiDescription This API endpoint posts a policy to the underlying BS.
-   * It can be used to create, update, and delete a slice on top of a BS.
+   * @apiDescription This API endpoint posts a new slice configuration to an
+   * underlying agent, specified as a JSON file with the format of the
+   * `sliceConfig` as contained in the `cellConfig` of an agent configuration.
+   * It can be used to create arbitrary slices with an arbitrary ID in the
+   * range [1,255] or to change slices in the range [0,255]. In the request, a
+   * slice ID must present. All other values will be copied from slice ID 0
+   * The caller should take care that the sum of slice percentages (i.e. of all
+   * present and added slices) should not exceed 100, as this is not catched at
+   * the controller but enforced at the MAC scheduler. The `stats` call should
+   * always be used after a call and sufficient time to verify the actions have
+   * been taken.
+   *
+   * @apiVersion v0.1.0
+   * @apiPermission None
+   * @apiExample Example usage:
+   *    curl -X POST http://127.0.0.1:9999/slice/enb/-1 --data-binary "@file"
+   *
+   * @apiParamExample {json} Request-Example:
+   *    {
+   *      "dl": [
+   *        {
+   *          "id": 0,
+   *          "percentage": 25,
+   *          "maxmcs": 28
+   *        },
+   *        {
+   *          "id": 3,
+   *          "percentage": 25,
+   *          "maxmcs": 28
+   *        }
+   *      ],
+   *      "ul": [
+   *        {
+   *          "id": 0,
+   *          "percentage": 25,
+   *          "maxmcs": 20
+   *        },
+   *        {
+   *          "id": 3,
+   *          "percentage": 25,
+   *          "maxmcs": 20
+   *        }
+   *      ]
+   *    }
+   *
+   * @apiSuccessExample Success-Response:
+   *    HTTP/1.1 200 OK
+   *
+   * @apiError BadRequest Mal-formed request or missing/wrong parameters,
+   * reported as JSON.
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "can not find agent" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "missing slice ID" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "Protobuf parser error" }
    */
   Pistache::Rest::Routes::Post(router, "/slice/enb/:id?",
       Pistache::Rest::Routes::bind(&flexran::north_api::flexible_sched_calls::apply_slice_config, this));
 
   /**
-   * @api {post} /rrm_config/ Post a user-defined RAN policy command, no body
-   * @apiName ApplyPolicyString
-   * @apiGroup user/slice/BS policies
+   * @api {post} /slice/enb/:id? Create a new pair of slices (short version)
+   * @apiName ApplySliceConfigurationShort
+   * @apiGroup SliceConfiguration
+   * @apiParam {Number} id The ID of the agent for which to change the slice
+   * configuration. This can be one of the following: -1 (last added agent),
+   * the eNB ID (in hex or decimal) or the internal agent ID which can be
+   g obtained through a `stats` call. Numbers smaller than 1000 are parsed as
+   * the agent ID.
+   * @apiParam {Number} slice_id The ID of the slices (UL and DL) to be created
+   * in the range [1,255].
    *
-   * @apiDescription This API endpoint posts a policy to the underlying BS.
-   * It can be used to create, update, and delete a slice on top of a BS.
+   * @apiDescription This API endpoint creates a new pair of slices copying the
+   * values of the slice 0.  It can be used to create arbitrary slices with an
+   * arbitrary ID in the range [1,255]. Please note that if slice 0 has already
+   * more than 50 percent, this call will fail (since the percentage value is
+   * copied, too). The caller should take care that the sum of slice
+   * percentages (i.e. of all present and added slices) should not exceed 100,
+   * as this is not catched at the controller but enforced at the MAC
+   * scheduler. The `stats` call should always be used after a call and
+   * sufficient time to verify the actions have been taken.
+   *
+   * @apiVersion v0.1.0
+   * @apiPermission None
+   * @apiExample Example usage:
+   *    curl -X POST http://127.0.0.1:9999/slice/enb/-1/slice/13
+   *
+   * @apiSuccessExample Success-Response:
+   *    HTTP/1.1 200 OK
+   *
+   * @apiError BadRequest Missing or wrong parameters, reported as JSON.
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "can not find agent" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "invalid slice ID" }
    */
   Pistache::Rest::Routes::Post(router, "/slice/enb/:id/slice/:slice_id",
       Pistache::Rest::Routes::bind(&flexran::north_api::flexible_sched_calls::apply_slice_config_short, this));
 
+  /**
+   * @api {delete} /slice/enb/:id? Delete slices
+   * @apiName DeleteSlice
+   * @apiGroup SliceConfiguration
+   * @apiParam {Number} [id=-1] The ID of the agent for which to change the
+   * slice configuration. This can be one of the following: -1 (last added
+   * agent), the eNB ID (in hex or decimal) or the internal agent ID which can
+   * be obtained through a `stats` call. Numbers smaller than 1000 are parsed as
+   * the agent ID.
+   *
+   * @apiDescription This API endpoint deletes slices as specified in the JSON
+   * data in the body specified as a JSON file with the format of the
+   * `sliceConfig` as contained in the `cellConfig` of a agent configuration
+   * for a given agent.  In particular, a valid slice ID must present and the
+   * percentage value must be zero. Slice 0 can not be removed.
+   *
+   * @apiVersion v0.1.0
+   * @apiPermission None
+   * @apiExample Example usage:
+   *    curl -X DELETE http://127.0.0.1:9999/slice/enb/-1 --data-binary "@file"
+   *
+   * @apiParamExample {json} Request-Example:
+   *    {
+   *      "dl": [
+   *        {
+   *          "id": 3,
+   *          "percentage": 0
+   *        }
+   *      ],
+   *      "ul": [
+   *        {
+   *          "id": 3,
+   *          "percentage": 0
+   *        }
+   *      ]
+   *    }
+   *
+   * @apiSuccessExample Success-Response:
+   *    HTTP/1.1 200 OK
+   *
+   * @apiError BadRequest Mal-formed request or missing/wrong parameters,
+   * reported as JSON.
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "can not find agent" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "missing slice ID" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "Protobuf parser error" }
+   */
   Pistache::Rest::Routes::Delete(router, "/slice/enb/:id?",
       Pistache::Rest::Routes::bind(&flexran::north_api::flexible_sched_calls::remove_slice_config, this));
 
+  /**
+   * @api {delete} /slice/enb/:id/slice/:slice_id Delete slices (short version)
+   * @apiName DeleteSliceShort
+   * @apiGroup SliceConfiguration
+   * @apiParam {Number} id The ID of the agent for which to change the
+   * slice configuration. This can be one of the following: -1 (last added
+   * agent), the eNB ID (in hex or decimal) or the internal agent ID which can
+   * be obtained through a `stats` call. Numbers smaller than 1000 are parsed as
+   * the agent ID.
+   * @apiParam {Number} slice_id The ID of the slices (UL and DL) to be created
+   * in the range [1,255].
+   *
+   * @apiDescription This API endpoint deletes the UL and DL slices with ID
+   * `slice_id` (only one ID can be provided for both slices) for a given agent.
+   *
+   * @apiVersion v0.1.0
+   * @apiPermission None
+   * @apiExample Example usage:
+   *    curl -X DELETE http://127.0.0.1:9999/slice/enb/-1/slice_id/3
+   *
+   * @apiSuccessExample Success-Response:
+   *    HTTP/1.1 200 OK
+   *
+   * @apiError BadRequest Mal-formed request or missing/wrong parameters,
+   * reported as JSON.
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "can not find agent" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "can not find DL slice ID" }
+   */
   Pistache::Rest::Routes::Delete(router, "/slice/enb/:id/slice/:slice_id",
       Pistache::Rest::Routes::bind(&flexran::north_api::flexible_sched_calls::remove_slice_config_short, this));
 
   /**
-   * @api {post} /ue_slice_assoc/ Post a user-defined UE-to-slice association
-   * @apiName ApplyPolicyString
-   * @apiGroup user/slice/BS policies
+   * @api {post} /ue_slice_assoc/enb/:id? Change the UE-slice association
+   * @apiName ChangeUeSliceAssociation
+   * @apiGroup SliceConfiguration
+   * @apiParam {Number} [id=-1] The ID of the agent for which to change the
+   * slice configuration. This can be one of the following: -1 (last added
+   * agent), the eNB ID (in hex or decimal) or the internal agent ID which can
+   * be obtained through a `stats` call. Numbers smaller than 1000 are parsed as
+   * the agent ID.
    *
-   * @apiDescription This API endpoint posts a policy to the underlying BS.
-   * It can be used to change the association of a UE to a slice. Currently,
-   * only the RNTI is supported.
+   * @apiDescription This API endpoint changes the association of a UE in an
+   * underlying agent, specified as a JSON file with the format of the
+   * `ueConfig` as contained in the agent configuration.  It can be used to
+   * changed the association of UEs using their current RNTI or IMSI. In the
+   * request, a slice ID and RNTI or IMSI must be present. The `stats` call
+   * should always be used after a call and sufficient time to verify the
+   * actions have been taken.
+   *
+   * @apiVersion v0.1.0
+   * @apiPermission None
+   * @apiExample Example usage:
+   *    curl -X POST http://127.0.0.1:9999/ue_slice_assoc/enb/-1 --data-binary "@file"
+   *
+   * @apiParamExample {json} Request-Example:
+   *    {
+   *      "ueConfig": [
+   *        {
+   *          "imsi": 208940100001115,
+   *          "dlSliceId": 3,
+   *          "ulSliceId": 3
+   *        }
+   *      ]
+   *    }
+
+   *
+   * @apiSuccessExample Success-Response:
+   *    HTTP/1.1 200 OK
+   *
+   * @apiError BadRequest Mal-formed request or missing/wrong parameters,
+   * reported as JSON.
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "can not find agent" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "no such slice" }
+   *
+   * @apiErrorExample Error-Response:
+   *    HTTP/1.1 400 BadRequest
+   *    { "error": "Protobuf parser error" }
    */
   Pistache::Rest::Routes::Post(router, "/ue_slice_assoc/enb/:id?",
       Pistache::Rest::Routes::bind(&flexran::north_api::flexible_sched_calls::change_ue_slice_assoc, this));
